a85f3483d5f260fa35808733380a88065f1272e6
Hubert <43858139+psychohub@users.noreply.github.com>
2024-02-25 22:44:06 -0600

    se crear el archivo index.ejs

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const adData = require('./src/data/anuncios.json');
 const adRoutes = require('./src/routes/adRoutes');
 const cors = require('cors');
 const errorHandler = require('./errorHandler');
+const path = require('path'); 
 const app = express();
 
 // Importa la configuración de mongoose
@@ -15,19 +17,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //routes
 
-app.get('/', (req, res) => {
-    res.send('Test node API');
+// Importar ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src/views'));
+app.use('/images', express.static(path.join(__dirname, 'src/images')));
+
+// Definir ruta para renderizar la vista
+app.get('/', (_req, res) => {
+    res.render('index', { adData: adData });
 });
 
-
-/**
 app.use('/api/ads', adRoutes);
- */
+ 
 
 
 connection.once('open', () => {
     app.listen(3000, () => {
         console.log('Servidor iniciado en el puerto 3000');
+        adData;
     });
 });
 
