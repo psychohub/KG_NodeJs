232e5dcf4ce596bbef30dcbfcef67b12fc4e376f
Hubert <43858139+psychohub@users.noreply.github.com>
2024-07-03 18:27:38 -0600

    Update connectMongoose.js

--- connectMongoose.js
+++ connectMongoose.js
@@ -1,8 +1,10 @@
-
 const mongoose = require('mongoose');
 
 // Configuración de mongoose y conexión a la base de datos
-mongoose.connect('mongodb://127.0.0.1/nodepop');
+mongoose.connect('mongodb://admin:password123@localhost:27017/parsedb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 // Conexión exitosa
 mongoose.connection.on('connected', () => {
