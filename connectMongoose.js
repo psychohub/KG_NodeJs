a4f8fcf4a7da0c4cada937f5938aa8ad467d9764
Hubert <43858139+psychohub@users.noreply.github.com>
2024-07-08 22:11:08 -0600

    Update connectMongoose.js

--- connectMongoose.js
+++ connectMongoose.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://admin:password123@127.0.0.1:27017/parsedb', {
+// Configuración de mongoose y conexión a la base de datos
+mongoose.connect('mongodb://admin:password123@localhost:27017/parsedb?authSource=admin', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
+// Conexión exitosa
 mongoose.connection.on('connected', () => {
     console.log('Conexión a la base de datos establecida con éxito');
 });
 
+// Error de conexión
 mongoose.connection.on('error', (err) => {
     console.error(`Error de conexión a la base de datos: ${err}`);
 });
 
 module.exports = { mongoose, connection: mongoose.connection };
-
